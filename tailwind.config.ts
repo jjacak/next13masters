5e62cc3c93c75b00fb00961af1eb293752c50354
Joanna Jacak <joanna.jacak@protonmail.com>
2023-09-10 12:46:27 +0200

    modify tailwind config and ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			gridTemplateColumns: {
+				"fill-1": "repeat(auto-fill, minmax(300px, 1fr))",
+			},
 			colors: {
 				"moon-mist": {
 					DEFAULT: "#E1E0D2",
