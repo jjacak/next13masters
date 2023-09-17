473c56cfbabc42c90dd5a2ead591320f55881021
Joanna Jacak <joanna.jacak@protonmail.com>
2023-09-17 14:36:56 +0200

    add active link component

---
+++ src/app/ui/atoms/ActiveLink.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import type { Route } from "next";
+import { type FC } from "react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { checkActiveLink } from "../utils";
+
+type ActiveLinkProps<T extends string> = {
+	children: React.ReactNode;
+	href: Route<T> | URL;
+	className?: string;
+	activeClassName?: string;
+};
+
+export const ActiveLink: FC<ActiveLinkProps<string>> = ({
+	children,
+	href,
+	className,
+	activeClassName,
+}) => {
+	const path = usePathname();
+	const isActive = checkActiveLink(path, href.toString());
+	return (
+		<Link href={href} className={clsx(isActive ? activeClassName : className)}>
+			{children}
+		</Link>
+	);
+};
--- src/app/ui/utils.ts
+++ src/app/ui/utils.ts
@@ -5,3 +5,9 @@ export const formatCurrency = (value: number) => {
     }).format(value);
 
 }
+
+export const checkActiveLink = (path: string, href: string) => {
+	const pathParts = path.split("/");
+	const hrefParts = href.split("/");
+	return pathParts[1] === hrefParts[1];
+};
