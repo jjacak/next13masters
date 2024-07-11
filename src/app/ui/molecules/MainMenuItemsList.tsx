58aad188c3d5777b6de339e4f26fd712c0356781
Joanna Jacak <joanna.jacak@protonmail.com>
2024-07-11 22:18:00 +0200

    add cart button and cart page

--- src/app/ui/molecules/MainMenuItemsList.tsx
+++ src/app/ui/molecules/MainMenuItemsList.tsx
@@ -1,17 +1,22 @@
 import type { FC } from "react";
 import { MAIN_MENU_ITEMS } from "../consts";
 import { ActiveLink } from "../atoms/ActiveLink";
+import CartButton from "../atoms/CartButton";
 
 export const MainMenuItemsList: FC = () => {
 	return (
 		<ul className="flex gap-3 justify-self-end">
-			{MAIN_MENU_ITEMS.map(({ name, path }) => (
-				<li key={path.toString()}>
+			{MAIN_MENU_ITEMS.map(({ name, path, icon }) => (
+				<li key={path.toString()} className="flex items-center justify-center">
 					<ActiveLink href={path} className="text-lg" activeClassName="text-lg font-bold">
+						{icon ? icon : null}
 						{name}
 					</ActiveLink>
 				</li>
 			))}
+			<li className="flex items-center justify-center">
+				<CartButton />
+			</li>
 		</ul>
 	);
 };
