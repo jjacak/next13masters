dde57245191f9976e62ad3b3161515533d72f6ed
Joanna Jacak <joanna.jacak@protonmail.com>
2024-07-20 22:22:15 +0200

    fix types

--- src/app/ui/atoms/ListItemDescription.tsx
+++ src/app/ui/atoms/ListItemDescription.tsx
@@ -1,13 +1,13 @@
 import type { FC } from "react";
-import type { ProductData } from "../types";
 import { formatCurrency } from "../utils";
+import type { ProductListItemFragment } from "@/gql/graphql";
 
-export const ListItemDescription: FC<ProductData> = ({ title, category, price }) => {
+export const ListItemDescription: FC<ProductListItemFragment> = ({ name, categories, price }) => {
 	return (
 		<div className="flex grow flex-col justify-between gap-3 p-5">
 			<div className="flex flex-col">
-				<h3 className="text-2xl font-bold ">{title}</h3>
-				<p className="text-lg text-cod-gray-200">{category}</p>
+				<h3 className="text-2xl font-bold ">{name}</h3>
+				<p className="text-lg text-cod-gray-200">{categories[0]?.name}</p>
 			</div>
 			<div className="flex flex-col">
 				<p className="mb-5 self-end text-lg font-bold">{formatCurrency(price / 100)}</p>
