3c3dcd809a3491d732ad6f838f44d772226d393a
Joanna Jacak <joanna.jacak@protonmail.com>
2024-07-22 22:59:10 +0200

    disable add to cart button while the form is submitting

--- src/app/ui/molecules/ProductDetails.tsx
+++ src/app/ui/molecules/ProductDetails.tsx
@@ -2,14 +2,14 @@ import type { FC } from "react";
 import { ProductDetailsImage } from "../atoms/ProductDetailsImage";
 import { ProductDetailsInformation } from "@/ui/atoms/ProductDetailsInformation";
 import type { ProductListItemFragment } from "@/gql/graphql";
-import { AddToCartButton } from "../atoms/AddToCartButton";
+import { AddToCartForm } from "./AddToCartForm";
 
 export const ProductDetails: FC<ProductListItemFragment> = (product) => {
 	return (
 		<article className="grid grid-cols-1  md:grid-cols-2">
 			<ProductDetailsImage name={product.name} images={product.images} />
 			<ProductDetailsInformation {...product} />
-			<AddToCartButton id={product.id} />
+			<AddToCartForm id={product.id} />
 		</article>
 	);
 };
